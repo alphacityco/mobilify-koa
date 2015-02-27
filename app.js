ca3b8cee093b6ff7e562823615ca96e154827924
p1nox <pinox101@gmail.com>
2015-02-27 10:23:34 -0300

    netive cluster support added

--- app.js
+++ app.js
@@ -3,6 +3,28 @@ require('newrelic');
 var app    = require('./src');
 var config = require('./src/config');
 
-app.listen(config.app.port, function(){
-  console.log('Koa listening on port ' + config.app.port);
-});
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+
+  cluster.on('listening', function(worker, address) {
+    console.log("A worker is now connected to " + address.address + ":" + address.port);
+  });
+
+  // Fork workers.
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('A worker died at: ' + worker.process.pid);
+  });
+
+} else {
+  app.listen(config.app.port, function(){
+    console.log('Koa listening on port ' + config.app.port);
+  });
+}
+
+
